2e234e71738f41e50d4d978998dea370cd44f946
SebasR-25 <seruprisatur2703@gmail.com>
2023-03-17 02:50:06 -0500

    Primer commit del segundo punto
    Se creó la clase view con todas sus funciones. Se creo la clase CarManage con la mayoría de sus funciones, donde se leen y escriben archivos binarios con la información de cada carro

---
+++ src/model/CarManage.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CarManage {
+    public int tempId;
+    private DataOutputStream dos;
+    private DataInputStream dis;
+
+    public String createFile() throws IOException {
+        String result = "";
+        try {
+            File file = new File("src/sources/" + "Vehicle_sequence.ddr");
+            if (file.createNewFile()) {
+                result = "Archivo creado: " + file.getName();
+            } else {
+                result = "El archivo ya existe.";
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error.");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void writeString(String cad, String fileNamePath) throws IOException {
+        // fileNamePath debe contener el nombre del archivo y la terminación del mismo
+        File file = new File(fileNamePath);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        dos = new DataOutputStream(fileOutputStream);
+        dos.writeUTF(cad);
+    }
+
+    public String readString(String fileNamePath) throws IOException {
+        // fileName debe contener el nombre del archivo y la terminación del mismo
+        File file = new File(fileNamePath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        dis = new DataInputStream(fileInputStream);
+        return dis.readUTF();
+    }
+
+    private boolean searchCar(int id) {
+        String fileNamePath = "/src/sources/vehiculo_" + id + ".veh";
+        File file = new File(fileNamePath);
+        return file.exists();
+    }
+
+    public String createCar(String marca, String modelo, String año, String matricula, String color, String id)
+            throws IOException {
+        String carInfo = marca + "," + modelo + "," + año + "," + matricula + "," + color + "," + id;
+        this.writeString(carInfo, "/src/sources/vehiculo_" + id + ".veh");
+        return "El carro ha sido creado correctamente";
+    }
+
+    public String showAllCars() throws IOException {
+        String result = "";
+        for (int i = 1; i == tempId; i++) {
+            String fileNamePath = "/src/sources/vehiculo_" + i + ".veh";
+            result = myToString(this.readString(fileNamePath), i) + "\n";
+        }
+        return result;
+    }
+
+    public String showACar(int carID, String carInfo) {
+        return this.myToString(carInfo, carID);
+    }
+
+    private String myToString(String carInfo, int carID) {
+        String result = "";
+        if (this.searchCar(carID)) {
+            carInfo.replace(',', '\n');
+        } else {
+            result = "El auto no existe";
+        }
+        return result;
+    }
+}
---
+++ src/view/View.java
@@ -0,0 +1,29 @@
+package view;
+import java.util.Scanner;
+
+public class View{
+    private Scanner input;
+
+    public View(){
+        input = new Scanner(System.in);
+    }
+    public void showMessage(String message){
+		System.out.println(message);
+	}
+    public String readString(String message){
+        this.showMessage(message);
+        return input.next();
+    }
+    public double readDouble(String message){
+		this.showMessage(message);
+		return input.nextDouble();
+	}
+    public short readShort(String message){
+		this.showMessage(message);
+		return input.nextShort();
+	}
+    public short readMenu(){
+        String menuText = "1. Agregar carro"+"\n"+"2. Mostrar todos los carros"+"\n"+"3. Mostrar un carro especifico"+"\n"+"4. Salir";
+        return readShort(menuText);
+    }
+}
